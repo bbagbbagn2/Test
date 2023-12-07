eb5ab25ab5a1285c16a8b417589fb26613f9ac31
bbagbbagn2 <pyoungh999@naver.com>
2023-12-07 18:30:04 +0900

    feat(Footer FooterHeader 컴포넌트 생성)

---
+++ src/literal-sauce-drip/Footer/FooterHeader/FooterHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import theme from '../../theme';
+
+export default function NavItem({ children }) {
+    return (
+            <NavItemHeading>{children}</NavItemHeading>
+    );
+};
+
+const NavItemHeading = styled.h1`
+    font-size: 1.5rem;
+    font-family: Montserrat-regular;
+    font-weight: 600;
+    line-height: 1.9375rem;
+
+    color: ${theme.colors.white};
+`;
--- src/literal-sauce-drip/Footer/NavItem/NavItem.jsx
+++ src/literal-sauce-drip/Footer/NavItem/NavItem.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import theme from '../../theme';
-export default function NavItem({ title, href, children }) {
+
+export default function NavItem({ href, children }) {
     return (
-        <li>
-            <NavItemHeading>{title}</NavItemHeading>
             <NavItemLink href={href}>
                 {children}
             </NavItemLink>
-        </li>
     );
 };
-
-const NavItemHeading = styled.h1`
-    font-size: 1.5rem;
-    font-family: Montserrat-regular;
-    font-weight: 600;
-    line-height: 1.9375rem;
-
-    color: ${theme.colors.white};
-`;
 
 const NavItemLink = styled.a`
     font-size: .75rem;
