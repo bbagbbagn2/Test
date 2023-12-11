60f5dec8ddbe7b4b07d455ebd764194e9d41d2fb
bbagbbagn2 <pyoungh999@naver.com>
2023-12-11 19:42:06 +0900

    fix (list grid 변경)

--- src/components/StyledList.js
+++ src/components/StyledList.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export default function Main({ title, price, description }) {
 
     return (
-        <>
+        <PriceBox>
             <ProductWrapper>
                 <Title>{title}</Title>
                 <Message>{description}</Message>
@@ -13,9 +13,14 @@ export default function Main({ title, price, description }) {
                 <Price>{price}</Price>
                 <BuyButton>BUY NOW</BuyButton>
             </PriceWrapper>
-            </>
+            </PriceBox>
     );
 }
+
+const PriceBox = styled.div`
+    position: relative;
+    bottom: 0;
+`
 const ProductWrapper = styled.div`
     margin-top: 0;
     height: 108px;
@@ -23,36 +28,29 @@ const ProductWrapper = styled.div`
 `;
 
 const Title = styled.p`
-    margin-top: 0;
-    margin-bottom: 0;
     color: #2b2835;
-    font-family: Montserrat;
+    font-family: Montserrat-medium;
     font-size: 50px;
-    font-weight: 500;
     line-height: 55px;
 `;
 
 const Message = styled.p`
-    margin-bottom: 0;
     color: #030303;
-    font-family: Montserrat;
+    font-family: Montserrat-regular;
     font-size: 14px;
-    font-weight: 400;
     line-height: 21.0145px;
     z-index: 1;
 `;
 
 const PriceWrapper = styled.div`
     margin-top: 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    justify-items: start;
+    display: flex;
+    column-gap: 29px;
     align-items: center;
 `;
 
 const Price = styled.p`
     color: #9D8220;
-    font-family: Montserrat;
     font-size: 25px;
     font-weight: 600;
 `;
@@ -60,14 +58,10 @@ const Price = styled.p`
 const BuyButton = styled.button`
     width: 145px;
     height: 41px;
-    border: none;
     border-radius: 24px;
-    background: #000000;
-    background-blend-mode: normal;
-    color: #ffffff;
-    font-family: Montserrat;
+    background: #000;
+    color: #fff;
+    font-family: Montserrat-semibold;
     font-size: 14px;
-    font-weight: 600;
     cursor: pointer;
-    z-index:0;
 `;
--- src/pages/ItemSection/ItemSection.jsx
+++ src/pages/ItemSection/ItemSection.jsx
@@ -12,21 +12,19 @@ export default function ItemListPage() {
                                 <Item key={product.id}>
                                     {product.id === 1 && (
                                         <ListWrapper>
-                                                <Image src={product.url} width="263px" top="-6px" marginbottom="-1px;" />
+                                                <Image src={product.url} />
                                                 <StyledList title={product.name} description={product.description} price={product.price} />
                                         </ListWrapper>
                                     )}
                                     {product.id === 2 && (
                                         <ListWrapper>
-                                            <ImageWrapper>
-                                                <Image src={product.url} width="319.35px" top="0" left="10px" marginbottom="70.41px" />
+                                                <Image src={product.url} width="319.35px" marginbottom="70.41px" />
                                                 <StyledList title={product.name} description={product.description} price={product.price} />
-                                            </ImageWrapper>
                                         </ListWrapper>)}
                                     {product.id === 3 && (
                                         <ListWrapper>
                                             <ImageWrapper>
-                                                <Image src={product.url} width="282px" top="17px" left="28px" marginbottom="21px" />
+                                                <Image src={product.url} marginbottom="21px" />
                                                 <StyledList title={product.name} description={product.description} price={product.price} />
                                             </ImageWrapper>
                                         </ListWrapper>
@@ -34,7 +32,7 @@ export default function ItemListPage() {
                                     {product.id === 4 && (
                                         <ListWrapper>
                                             <ImageWrapper>
-                                                <Image src={product.url} width="229px" top="10px" left="102px" marginbottom="-134px" />
+                                                <Image src={product.url} width="229px" top="10px" left="50px" marginbottom="-134px" />
                                                 <StyledList title={product.name} description={product.description} price={product.price} />
                                             </ImageWrapper>
                                         </ListWrapper>
@@ -50,7 +48,7 @@ export default function ItemListPage() {
                                     {product.id === 6 && (
                                         <ListWrapper>
                                             <ImageWrapper>
-                                                <Image src={product.url} width="231px" top="95px" left="94px" marginbottom="-106px" />
+                                                <Image src={product.url} width="231px" top="95px" left="64px" marginbottom="-50px" />
                                                 <StyledList title={product.name} description={product.description} price={product.price} />
                                             </ImageWrapper>
                                         </ListWrapper>
@@ -68,27 +66,26 @@ const Section = styled.section`
 
 const ItemList = styled.ul`
     width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    row-gap: 2.125em;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(393px, 393px));
     justify-content: space-between;
+    row-gap: 2.125em;
 `;
 
 const Item = styled.li`
     position: relative;
-    flex: 0 1 calc((100% - 4.685em) / 3);
     background: #f8f8f9;
     overflow: hidden;
-    
 `;
 
 const ListWrapper = styled.div`
     padding: 0 2.9375em;
+    padding-bottom: 2.3125em;
     width: 393px;
     height: 560px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-end;
     text-align: left;
 `;
 
@@ -99,9 +96,10 @@ const ImageWrapper = styled.div`
 const Image = styled.img`
     position: relative;
     z-index: 0;
+
     width: ${(props) => props.width};
     top: ${(props) => props.top};
     left: ${(props) => props.left};
-    margin-bottom: ${(props) => props.marginbottom || "auto"};
+    margin-bottom: ${(props) => props.marginbottom};
 `;
 
