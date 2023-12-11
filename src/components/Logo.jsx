34e9c93a6254edbe82fa9f542496ae436ae678ae
bbagbbagn2 <pyoungh999@naver.com>
2023-12-11 21:29:57 +0900

    docs(Logo.js->jsx로 파일 확장자 변경) / refactor(Logo.jsx 리팩토링)

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import { ReactComponent as SvgLogo } from '../logos/logo.svg';
 
 export default function Logo() {
     return (
-        <a href="/"><Svg src={SvgLogo} alt="logo" /></a>
+        <a href="/">
+            <SvgLogo src={SvgLogo} alt="logo" />
+        </a>
     );
 }
-
-const Svg = styled(SvgLogo)``;
